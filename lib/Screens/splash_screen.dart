d2bbb132b72f35e9bf812d3cf6566c8c2bdf2689
Syed Huzaifa Ali <syedhuzaiafali1998@gmail.com>
2021-10-02 17:31:32 +0500

    Splash Screen added and app bar color changed

---
+++ lib/Screens/splash_screen.dart
@@ -0,0 +1,109 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+//  import 'package:splashscreen/splashscreen.dart';
+ import 'package:teb_bar/Screens/app_main_screen';
+
+
+class SplashScreenWork extends StatefulWidget {
+  const SplashScreenWork({ Key? key }) : super(key: key);
+
+  @override
+  _SplashScreenWorkState createState() => _SplashScreenWorkState();
+}
+
+class _SplashScreenWorkState extends State<SplashScreenWork> {
+  @override@override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 6),
+          ()=>Navigator.pushReplacement(context,MaterialPageRoute(builder:(context) => 
+                                                          const AppScreen(),
+                                                         )
+                                       )
+         );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return
+
+    Container(
+      color: Colors.teal,
+      child:Image.asset("assets/images/splash_screen.jpeg",fit: BoxFit.cover,
+    height: double.infinity,
+    width: double.infinity,),
+    );
+  }
+}
+
+
+
+// class SplashScreenWork extends StatelessWidget {
+//   const SplashScreenWork({
+//     Key? key,
+//   }) : super(key: key);
+//   // @override
+//   // void initState() {
+//   //   super.initState();
+//   //   Timer(Duration(seconds: 3),
+//   //         ()=>Navigator.pushReplacement(context,MaterialPageRoute(builder:(context) => 
+//   //                                                         const AppScreen(),
+//   //                                                        )
+//   //                                      )
+//   //        );
+//   // }
+//   // @override
+//   // Widget build(BuildContext context) {
+//   //   return Container(
+//   //     color: Colors.white,
+//   //     child:FlutterLogo(size:MediaQuery.of(context).size.height)
+//   //   );
+//   // }
+// }
+// }
+
+// import 'dart:async';
+// import 'package:flutter/material.dart';
+// import 'package:splashscreen/splashscreen.dart';
+// void main() {
+// runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+// @override
+// Widget build(BuildContext context) {
+// 	return MaterialApp(
+// 	title: 'Splash Screen',
+// 	theme: ThemeData(
+// 		primarySwatch: Colors.green,
+// 	),
+// 	home: Splash2(),
+// 	debugShowCheckedModeBanner: false,
+// 	);
+// }
+// }
+// class Splash2 extends StatelessWidget {
+// @override
+// Widget build(BuildContext context) {
+// 	return SplashScreen(
+// 	seconds: 6,
+// 	navigateAfterSeconds: new SecondScreen(),
+// 	title: new Text('GeeksForGeeks',textScaleFactor: 2,),
+// 	image: new Image.network('https://www.geeksforgeeks.org/wp-content/uploads/gfg_200X200.png'),
+// 	loadingText: Text("Loading"),
+// 	photoSize: 100.0,
+// 	loaderColor: Colors.blue,
+// 	);
+// }
+// }
+// class SecondScreen extends StatelessWidget {
+// @override
+// Widget build(BuildContext context) {
+// 	return Scaffold(
+// 	appBar: AppBar(title:Text("GeeksForGeeks")),
+// 	body: Center(
+// 		child:Text("Home page",textScaleFactor: 2,)
+// 	),
+// 	);
+// }
+// }
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:teb_bar/Screens/call_screen.dart';
-import 'Screens/chat_screen.dart';
-import 'Screens/status_screen.dart';
+import 'package:teb_bar/Screens/splash_screen.dart';
+
+
 
 void main() {
   runApp(const MyApp());
@@ -20,62 +20,7 @@ class MyApp extends StatelessWidget {
         
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Galaxy Messenger'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    
-    return DefaultTabController(
-        length: 4,
-        initialIndex: 1,
-        child: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Color.fromRGBO(195,132,0,0),
-            bottom: const TabBar(
-              labelStyle: TextStyle(fontSize:20),
-              indicatorColor: Colors.amberAccent,
-              
-
-              tabs: [
-                Tab(icon: Icon(Icons.search),),
-                Tab(text: 'Chat',),
-                Tab(text: 'Status',),
-                Tab(text: 'Call',),
-              ],
-            ),
-            title: const Text('Galexy Messenger'),
-          ),
-          body: const TabBarView(
-            children: [
-              Icon(Icons.search ),
-              ChatsScreen(),
-              StatsScreen(),
-              CallScreen(),
-            ],
-          ),
-      
-      ),// This trailing comma makes auto-formatting nicer for build methods.
+      home: const SplashScreenWork(),
     );
   }
 }
